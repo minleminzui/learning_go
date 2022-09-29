87df442ef7c92b1a40dbc1b5cb62185eb897f7d6
yitong <2969413251@qq.com>
2022-09-29 16:13:54 +0800

    finish gee-day6

---
+++ gee_learn_from_geektutu/day6-template/gee/trie.go
@@ -0,0 +1,84 @@
+package gee
+
+import (
+	"fmt"
+	"strings"
+)
+
+type node struct {
+	pattern  string
+	part     string
+	isWild   bool
+	children []*node
+}
+
+func (n *node) String() string {
+	return fmt.Sprintf("node{pattern=%s, part=%s, isWild=%t}", n.pattern, n.part, n.isWild)
+}
+
+// 用来insert使用
+func (n *node) matchChild(part string) *node {
+	for _, child := range n.children {
+		if child.part == part || child.isWild {
+			return child
+		}
+	}
+
+	return nil
+}
+
+func (n *node) insert(parttern string, parts []string, height int) {
+	if len(parts) == height {
+		n.pattern = parttern
+		return
+	}
+	part := parts[height]
+	child := n.matchChild(part)
+	if child == nil {
+		child = &node{part: part, isWild: part[0] == '*' || part[0] == ':'}
+		n.children = append(n.children, child)
+	}
+	child.insert(parttern, parts, height+1)
+}
+
+// 多叉树先序遍历，获取所有pattern node
+func (n *node) travel(list *([]*node)) {
+	if n.pattern != "" {
+		*list = append(*list, n)
+	}
+
+	for _, child := range n.children {
+		child.travel(list)
+	}
+}
+
+func (n *node) matchChildren(part string) []*node {
+	nodes := make([]*node, 0)
+	for _, child := range n.children {
+		if child.part == part || child.isWild {
+			nodes = append(nodes, child)
+		}
+	}
+
+	return nodes
+}
+
+func (n *node) search(parts []string, height int) *node {
+	if len(parts) == height || strings.HasPrefix(n.part, "*") {
+		if n.pattern == "" {
+			return nil
+		}
+		return n
+	}
+	part := parts[height]
+	children := n.matchChildren(part)
+
+	for _, child := range children {
+		result := child.search(parts, height+1)
+		if result != nil {
+			return result
+		}
+	}
+
+	return nil
+}
